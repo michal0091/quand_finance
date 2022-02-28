6879917a197d896447a0e27c70a1b88cb62be5e8
michal0091 <michal.kinel@gmail.com>
2022-02-28 22:26:56 +0100

    initial commit

---
+++ main.R
@@ -0,0 +1,10 @@
+# quand_finance -----------------------------------------------------------
+
+
+# libaries ----------------------------------------------------------------
+library(data.table)
+library(zoo)
+library(tidyquant)
+library(tidyverse)
+
+
